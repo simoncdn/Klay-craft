08399688ef9250f72cf3558a5d0f8f762ac2b171
sdn <cdna.simon@gmail.com>
2024-02-28 23:21:42 +0100

    ➕ feat(project-config): Add prettier/eslint dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
 	devServer: {
 		port: 3008,
 	},
-	modules: ['@nuxtjs/tailwindcss', 'nuxt-icon'],
+	modules: ['@nuxtjs/eslint-module', '@nuxtjs/tailwindcss', 'nuxt-icon'],
 	tailwindcss: {
 		cssPath: '~/assets/css/tailwind.css',
 		configPath: 'tailwind.config.js',
