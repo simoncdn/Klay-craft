53056caf25b2e84f93f7a57ec00e027c0f8063cc
sdn <cdna.simon@gmail.com>
2024-03-02 00:13:05 +0100

    ✨ feat(utils): Add formatPrice utils function

--- utils/index.ts
+++ utils/index.ts
@@ -4,3 +4,10 @@ import { twMerge } from 'tailwind-merge'
 export const cn = (...classes: ClassValue[]) => {
 	return twMerge(clsx(...classes))
 }
+
+export const formatPrice = (price: number) => {
+	return new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD'
+	}).format(price)
+}
