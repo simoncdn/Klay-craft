dfd4ca4c2fa9194e05d55add75e6377d51abc9cd
sdn <cdna.simon@gmail.com>
2024-03-01 09:02:33 +0100

    ✨ feat(app): Add WIP message

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <script setup></script>
 
 <template>
-	<NuxtPage />
+	<!-- <NuxtPage /> -->
+
+	<div class="w-full h-screen flexCenter text-6xl font-heading">
+		🚧 Coming soon 🚧
+	</div>
 </template>
