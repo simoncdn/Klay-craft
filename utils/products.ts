9267a7a2721586cc924d654cd594edb76253395a
sdn <cdna.simon@gmail.com>
2024-03-02 00:12:10 +0100

    ✨ feat(utils): Add fakeData

---
+++ utils/products.ts
@@ -0,0 +1,26 @@
+export const products = [
+	{
+		id: 0,
+		name: 'APHRODITE NO.1',
+		description: 'Body vase 18"',
+		price: 64,
+		image: '/images/aphrodite_no1.png',
+		quantity: 0
+	},
+	{
+		id: 1,
+		name: 'PLATE NO.1',
+		description: 'Everyday dinner plate',
+		price: 18,
+		image: '/images/plate_no1.png',
+		quantity: 0
+	},
+	{
+		id: 2,
+		name: 'SET NO.4',
+		description: 'Set 2 plates, 1 cup',
+		price: 40,
+		image: '/images/set_no4.png',
+		quantity: 0
+	}
+]
