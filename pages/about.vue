5a8df64fcb71f03e4f5d3caf2dbf022f3bf23ced
sdn <cdna.simon@gmail.com>
2024-03-03 16:29:21 +0100

    ✨ feat(page): import about sections to the page

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,37 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div>ABOUT</div>
+	<div :class="cn('h-full w-full', 'gap-32', 'flexCol')">
+		<AboutHeroSection />
+		<AboutSashaSection />
+		<AboutSkylarSection />
+
+		<div :class="cn('w-full h-screen', 'flexCenter')">
+			<NuxtImg
+				src="/images/skylar_benett_2.jpg"
+				alt="About"
+				:class="
+					cn(
+						'w-full h-5/6',
+						'object-cover',
+						'grayscale',
+						'hover:grayscale-0 transition-all duration-300'
+					)
+				"
+			/>
+			<NuxtImg
+				src="/images/sasha_reed_2.jpg"
+				alt="About"
+				:class="
+					cn(
+						'w-full h-5/6',
+						'object-cover',
+						'grayscale',
+						'hover:grayscale-0 transition-all duration-300'
+					)
+				"
+			/>
+		</div>
+		<LayoutFooter />
+	</div>
 </template>
