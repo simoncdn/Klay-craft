245cf4bb2bdf128bb01cead389fd51f998beffaf
sdn <cdna.simon@gmail.com>
2024-03-03 17:34:02 +0100

    🚧 feat(component, page): Create productBody

---
+++ components/Products/ProductsBody.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div :class="cn('h-full w-full', 'paddingX gap-4', 'flexCol')">
+		<div :class="cn('w-full h-16', 'bg-purple-100')">Toolbar</div>
+
+		<ul :class="cn('h-fit', 'gap-4', 'grid grid-cols-4 auto-rows-fit')">
+			<li
+				v-for="product in products"
+				:key="product.id"
+				:class="cn('h-full w-full', 'gap-2', 'flexCol')"
+			>
+				<NuxtLink
+					:to="`/products/${product.id}`"
+					:class="cn('relative group', 'w-full h-full')"
+				>
+					<NuxtImg
+						:src="product.image"
+						:alt="product.name"
+						:class="
+							cn(
+								'w-full h-full',
+								'object-cover',
+								'group-hover:brightness-75 transition-all duration-300'
+							)
+						"
+					/>
+					<Icon
+						name="lucide:eye"
+						:class="
+							cn(
+								'absolute top-0 right-0 left-0 bottom-0 opacity-0',
+								'm-auto',
+								'text-2xl text-beige',
+								'group-hover:opacity-100 transition-all duration-300'
+							)
+						"
+					/>
+				</NuxtLink>
+
+				<div :class="cn('h-full', 'flexCol')">
+					<div :class="cn('flexBetween items-center', 'txt-sm')">
+						<h6>{{ product.name }}</h6>
+						<span class="font-medium">{{ formatPrice(product.price) }}</span>
+					</div>
+					<p class="txt-xs text-mistyGreen">{{ product.description }}</p>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import LayoutFooter from '~/components/Layout/LayoutFooter.vue'
+import ProductsBody from '~/components/Products/ProductsBody.vue'
 import ProductsHeroSection from '~/components/Products/ProductsHeroSection.vue'
 </script>
 
 <template>
-	<div :class="cn('h-full w-full', 'gap-16', 'flexCol')">
+	<div :class="cn('h-full w-full', 'gap-32', 'flexCol')">
 		<ProductsHeroSection />
-		<!-- <LayoutFooter /> -->
+		<ProductsBody />
+		<LayoutFooter />
 	</div>
 </template>
