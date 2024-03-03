f03c7eb5174eca8e41f7b72bd89b09524125a559
sdn <cdna.simon@gmail.com>
2024-03-03 11:06:17 +0100

    ✨ feat(component): Add productOverview section

--- components/Home/ProductOverviewSection.vue
+++ components/Home/ProductOverviewSection.vue
@@ -17,32 +17,61 @@ const translateX = computed(() => {
 </script>
 
 <template>
-	<div :class="cn('section', 'py-16 gap-16 bg-red-200', 'flexCol')">
-		<h3 class="text-center">
+	<div :class="cn('section', 'py-16 gap-16', 'flexCol')">
+		<h2 class="text-center h-fit">
 			New in <br />
 			Selection
-		</h3>
+		</h2>
 
-		<div :class="cn('h-fit w-full', 'gap-8', 'flex items-center bg-red-300')">
-			<div :class="cn('h-full w-fit bg-blue-300', 'flexCenter')">
+		<div :class="cn('h-full w-full', 'gap-8', 'flex flex-row items-start')">
+			<div :class="cn('h-full w-fit', 'flexCenter')">
 				<h3 :class="cn('-rotate-90', 'text-center')">
-					<router-link to="/products" class="hover:underline">
+					<router-link to="/products" class="hover:opacity-70">
 						Shop all <br />
 						ceramics
 					</router-link>
 				</h3>
 			</div>
-			<div ref="productsList" :class="cn(
-				'relative scrollbar-none w-full overflow-x-auto',
-				'flexCol justify-end items-start'
-			)
-				">
-				<ul :class="cn('gap-4', 'flex flex-row')">
-					<li v-for="product in products" :key="product.id" class="w-64 h-96">
-						<router-link :to="`/product/${product.id}`" :class="cn('h-full', 'gap-4', 'flexCol')">
-							<img :src="product.image" :alt="product.name" class="w-full h-full object-cover" />
+			<div :class="cn('relative overflow-x-hidden', 'w-full h-full')">
+				<ul
+					ref="productsList"
+					:class="cn('overflow-x-auto scrollbar-none', 'min-w-0 w-full', 'gap-4', 'flex')"
+				>
+					<li
+						v-for="product in products"
+						:key="product.id"
+						class="w-64 h-96 flex-shrink-0"
+					>
+						<router-link
+							:to="`/product/${product.id}`"
+							:class="cn('h-full group', 'gap-4', 'flexCol')"
+						>
+							<div class="relative h-full w-full">
+								<img
+									:src="product.image"
+									:alt="product.name"
+									:class="
+										cn(
+											'w-full h-full',
+											'object-cover',
+											'group-hover:brightness-75 transition-all duration-150'
+										)
+									"
+								/>
+								<Icon
+									name="lucide:eye"
+									:class="
+										cn(
+											'absolute top-0 right-0 left-0 bottom-0 opacity-0',
+											'm-auto',
+											'text-3xl text-beige',
+											'group-hover:opacity-100 transition-all duration-150'
+										)
+									"
+								/>
+							</div>
 
-							<div :class="cn('gap-1', 'flexCol')">
+							<div :class="cn('flexCol')">
 								<div :class="cn('flexBetween items-center', 'txt-sm')">
 									<span>{{ product.name }}</span>
 									<span class="font-medium">{{
@@ -57,9 +86,15 @@ const translateX = computed(() => {
 					</li>
 				</ul>
 
-				<div :class="cn('absolute left-0 bottom-0', 'w-4/12 bg-lightGray', 'mt-16', 'flex')">
-					<span :class="cn('w-4/12 h-[2px]', 'bg-carbon')"
-						:style="{ transform: `translateX(${translateX}px)` }"></span>
+				<div
+					:class="
+						cn('absolute left-0 -bottom-0', 'w-4/12 bg-lightGray', 'm-auto', 'flex')
+					"
+				>
+					<span
+						:class="cn('w-4/12 h-[2px]', 'bg-carbon')"
+						:style="{ transform: `translateX(${translateX}px)` }"
+					></span>
 				</div>
 			</div>
 		</div>
